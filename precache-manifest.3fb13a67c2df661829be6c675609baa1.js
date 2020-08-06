1ebff17ad142d6aa62db4df08b5a4c5f51046382
Alex Malichenko <malichenko-ov@ukrposhta.ua>
2020-08-06 20:27:21 +0300

    Updates

---
+++ precache-manifest.3fb13a67c2df661829be6c675609baa1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a47254468f83399f0a27d864262a49",
+    "url": "/health-tracker/index.html"
+  },
+  {
+    "revision": "10432348f0b18c3b355f",
+    "url": "/health-tracker/static/css/main.c1755be6.chunk.css"
+  },
+  {
+    "revision": "b86b2eff83b764590004",
+    "url": "/health-tracker/static/js/2.8e92429b.chunk.js"
+  },
+  {
+    "revision": "11da4d0289ce192632a3fc6590a86fde",
+    "url": "/health-tracker/static/js/2.8e92429b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10432348f0b18c3b355f",
+    "url": "/health-tracker/static/js/main.16869357.chunk.js"
+  },
+  {
+    "revision": "5d131cb1b0a361490cef",
+    "url": "/health-tracker/static/js/runtime-main.a6413ab2.js"
+  },
+  {
+    "revision": "595c39355d964742235fc51a469f1908",
+    "url": "/health-tracker/static/media/female.595c3935.png"
+  },
+  {
+    "revision": "c9a4cbd6aa9353018ec3773d16c960a5",
+    "url": "/health-tracker/static/media/login-background.c9a4cbd6.png"
+  },
+  {
+    "revision": "048e835ed0b014cec5c98f55fa044b07",
+    "url": "/health-tracker/static/media/male.048e835e.png"
+  },
+  {
+    "revision": "fc358c8d8fd0b88298e75bba6458d4e8",
+    "url": "/health-tracker/static/media/man.fc358c8d.png"
+  },
+  {
+    "revision": "086c9631c73d854a33ab6f4319f76817",
+    "url": "/health-tracker/static/media/registration-background.086c9631.png"
+  },
+  {
+    "revision": "a0cf86673598422ecaf61dfbca1e2f12",
+    "url": "/health-tracker/static/media/woman.a0cf8667.png"
+  }
+]);
